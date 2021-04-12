b4d5b34dab34815c6d7b248fe800fae1a57c1020
YannouRavoet <yannou.ravoet@gmail.com>
2021-04-12 13:31:08 +0200

    added pong deepproblog

--- resources/js/projectmanager.js
+++ resources/js/projectmanager.js
@@ -29,58 +29,20 @@ const tags = {
     COMPUTERVISION: 'Computer Vision',
     CPP: 'C++',
     PATHFINDING: 'Pathfinding',
-    JPS: 'Jump Point Search'
+    JPS: 'Jump Point Search',
+    FOL: 'First Order Logic',
+    DEEPPROBLOG: 'DeepProbLog'
 }
 
 const allProjects = [
     new Project(
-        "TopDownShooter",
-        [tags.UNITY, tags.UNITY],
-        "https://github.com/YannouRavoet/TopDownShooter",
-        "resources/images/projects/TopDownShooter.png",
-        "A simple game where you battle against waves of enemies with varying weapons (Unity)",
-        2019,
-        "https://yannouravoet.github.io/UnityShooter/"),
-    new Project(
-        "OpenAI Taxi",
-        [tags.PYTHON, tags.RF, tags.OPENAI],
-        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Taxi_v3.py",
-        "resources/images/projects/OpenAI_Taxi.png",
-        "Q-learning for discrete state spaces",
-        2019,
-        "https://YannouRavoet.github.io/gymprojects#QLearning"),
-    new Project(
-        "OpenAI Cartpole",
-        [tags.PYTHON, tags.RF, tags.OPENAI],
-        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Cartpole_v0.py",
-        "resources/images/projects/OpenAI_Cartpole.png",
-        "Deep Q-learning for continuous state spaces",
-        2019,
-        "https://YannouRavoet.github.io/gymprojects#DeepQ-Network"),
-    new Project(
-        "OpenAI Breakout",
-        [tags.PYTHON, tags.RF, tags.OPENAI],
-        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Breakout.py",
-        "resources/images/projects/OpenAI_Breakout.png",
-        "Reinforcement Learning on Atari Games using the OpenAI framework",
-        2020,
-        "https://YannouRavoet.github.io/gymprojects#DoubleDeepQ-Network"),
-    new Project(
-        "Kaggle Titanic",
-        [tags.PYTHON, tags.KAGGLE],
-        "https://github.com/YannouRavoet/titanic",
-        "resources/images/projects/Kaggle_Titanic.png",
-        "An introduction to data visualization and data wrangling",
-        2020,
-        "https://yannouravoet.github.io/titanic"),
-    new Project(
-        "OpenSpiel Poker",
-        [tags.PYTHON, tags.RF, tags.OPENSPIEL],
-        "https://github.com/YannouRavoet/ml_project",
-        "resources/images/projects/Poker.png",
-        "An application of game theory combined with reinforcement learning using the DeepMind OpenSpiel library",
-        2020,
-        "https://github.com/YannouRavoet/ml_project"),
+        "Pong in DeepProblog",
+        [tags.DEEPPROBLOG, tags.CNN, tags.FOL],
+        "https://github.com/YannouRavoet/Pong_DeepProbLog",
+        "resources/images/projects/Pong_DeepProblog.gif",
+        "An application of Deep Problog to the classic Pong Game testing the combination of neural networks and Logic Programming to divide perception and decision logic.",
+        2021,
+        "https://github.com/YannouRavoet/Pong_DeepProbLog"),
     new Project(
         "Sign Language Recognition",
         [tags.PYTHON, tags.OPENPOSE, tags.CNN, tags.LSTM, tags.COMPUTERVISION],
@@ -98,25 +60,86 @@ const allProjects = [
         2018,
         "https://vimeo.com/327167924"),
     new Project(
+        "OpenAI Breakout",
+        [tags.PYTHON, tags.RF, tags.OPENAI],
+        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Breakout.py",
+        "resources/images/projects/OpenAI_Breakout.gif",
+        "Reinforcement Learning on Atari Games using the OpenAI framework",
+        2020,
+        "https://YannouRavoet.github.io/gymprojects#DoubleDeepQ-Network"),
+    new Project(
+        "OpenAI Cartpole",
+        [tags.PYTHON, tags.RF, tags.OPENAI],
+        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Cartpole_v0.py",
+        "resources/images/projects/OpenAI_Cartpole.gif",
+        "Deep Q-learning for continuous state spaces",
+        2019,
+        "https://YannouRavoet.github.io/gymprojects#DeepQ-Network"),
+    new Project(
+        "OpenAI Taxi",
+        [tags.PYTHON, tags.RF, tags.OPENAI],
+        "https://github.com/YannouRavoet/gymprojects/blob/master/src/Taxi_v3.py",
+        "resources/images/projects/OpenAI_Taxi.gif",
+        "Q-learning for discrete state spaces",
+        2019,
+        "https://YannouRavoet.github.io/gymprojects#QLearning"),
+    new Project(
+        "Kaggle Titanic",
+        [tags.PYTHON, tags.KAGGLE],
+        "https://github.com/YannouRavoet/titanic",
+        "resources/images/projects/Kaggle_Titanic.png",
+        "An introduction to data visualization and data wrangling",
+        2020,
+        "https://yannouravoet.github.io/titanic"),
+    new Project(
+        "OpenSpiel Poker",
+        [tags.PYTHON, tags.RF, tags.OPENSPIEL],
+        "https://github.com/YannouRavoet/ml_project",
+        "resources/images/projects/Poker.png",
+        "An application of game theory combined with reinforcement learning using the DeepMind OpenSpiel library",
+        2020,
+        "https://github.com/YannouRavoet/ml_project"),
+    new Project(
         "Pathfinding Project",
         [tags.CPP, tags.PATHFINDING, tags.JPS],
         "https://github.com/YannouRavoet/MediaProcessing_PathfindingGame_2018",
         "resources/images/projects/MP_Pathfinding_2018.png",
         "Jump Point Search implementation with enemies and healthpacks",
         2018,
-        "https://github.com/YannouRavoet/MediaProcessing_PathfindingGame_2018")
+        "https://github.com/YannouRavoet/MediaProcessing_PathfindingGame_2018"),
+    new Project(
+        "TopDownShooter",
+        [tags.UNITY, tags.UNITY],
+        "https://github.com/YannouRavoet/TopDownShooter",
+        "resources/images/projects/TopDownShooter.png",
+        "A simple game where you battle against waves of enemies with varying weapons (Unity)",
+        2019,
+        "https://yannouravoet.github.io/UnityShooter/")
 ]
 
 /*=====================*/
 /*DISPLAY FUNCTIONALITY*/
 /*=====================*/
-const projects_per_row = 5;
+
+function getProjectsPerRow(){
+    var width = window.innerWidth;
+    if(width > 1450)
+        return 5;
+    else if(width > 1200)
+        return 4;
+    else if(width > 1000)
+        return 3;
+    else if(width > 600)
+        return 2;
+    return 1;
+}
 
 function displayProjects(projects){
+    $("#projects_div").empty();
     let index = 0;
     let current_row = null;
     projects.forEach(project => {
-        if (index % projects_per_row === 0)
+        if (index % getProjectsPerRow() === 0)
             current_row = makeProjectRow();
         let projectBox = makeProjectBox(project);
         current_row.appendChild(projectBox);
